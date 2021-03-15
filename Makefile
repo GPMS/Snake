82e39da7f3e66dd855731b7b10f16a676411aada
GPMS <gab.portela@hotmail.com>
2021-03-15 17:27:27 -0300

    Switched from plain Makefiles to CMake and build scripts

--- Makefile
+++
@@ -1,8 +0,0 @@
-CC= gcc
-FLAGS= -g -Wall -std=c99 -pedantic
-LIBS= -lm -lSDL2 -lSDL2_image -lSDL2_ttf
-SRC= src/*.c src/Engine/*.c
-OUTPUT= game
-
-make:
-	$(CC) -o $(OUTPUT) $(FLAGS) $(LIBS) $(SRC)
---
+++ Scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+config=`cat Scripts/config.txt`
+
+if [ ! "$config" = "Debug" -a ! "$config" = "Release" ]; then
+    echo "Unknown build type"
+    exit 1
+fi
+echo "Building in $config mode"
+
+buildDir="build/$config"
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$config -B $buildDir
+cp -R resources $buildDir
+cd $buildDir && make
+
